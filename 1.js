350fe47ec6dbce97101151cffeb0995d3e1423d5
alexxx113 <lead1100@ya.ru>
2023-08-30 03:33:06 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  if (Lampa.Platform.is('android') && typeof WebAssembly !== 'undefined')
+	Lampa.Utils.putScriptAsync(['https://bwa.to/s'], function () {});
+	
+  Lampa.Utils.putScriptAsync(['http://sisi.am/nyam.serv.js?v21'], function () {});
+
+})();
