3d2a5e5358bc3784cbd8291f7e10821386e63749
alexxx113 <lead1100@ya.ru>
2023-08-30 03:34:44 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,6 +4,6 @@
   if (Lampa.Platform.is('android') && typeof WebAssembly !== 'undefined')
 	Lampa.Utils.putScriptAsync(['https://bwa.to/s'], function () {});
 	
-  Lampa.Utils.putScriptAsync(['http://sisi.am/nyam.serv.js?v21'], function () {});
+  Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/alexxx113/testt/main/2.js?v21'], function () {});
 
 })();
